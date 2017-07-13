5a73502792db91b4e936934b088b6dd003d60f29
Angel <bubblicious712@gmail.com>
2017-07-12 22:49:32 -0400

    Update Main.php

--- src/Angel/WLReason/Main.php
+++ src/Angel/WLReason/Main.php
@@ -1,2 +1,21 @@
 <?php
 
+namespace Angel\WLReason;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->saveDefaultConfig();
+  }
+  
+  public function onLogin(\pocketmine\event\player\PlayerJoinEvent $ev){
+    if(!$this->getServer()->isWhitelisted(($p = $ev->getPlayer()->getName()))){
+      $p->kick($this->getConfig()->get("reason"), false);
+    }
+  }
+}
